6b2b1e9fa609a6d43acdca757541221bbc402c62
abrar189 <algourabrar@gmail.com>
2021-04-26 15:27:42 +0300

    done

--- js/java.js
+++ js/java.js
@@ -2,4 +2,38 @@
 let userName = prompt('what is your name?');
 alert('welcome ' + userName);
 
+let ask1 = prompt('do you like football?');
+if (ask1 == 'yes' || ask1 == 'y' ){
+    console.log('yes');
+    alert('your Answer is yes ');
+} else {
+    alert('your Answer is no');}
 
+
+let ask2 = prompt('Is your favorite club is Real Madrid?');
+    console.log('yes');
+if (ask2 == 'yes' || ask2 == 'y' ){
+    alert('your Answer is yes ');
+} else {
+    alert('your Answer is no');}
+    
+  let aks3 = prompt('Is Cristiano Ronaldo your favorite player?');
+    if (aks3 == 'yes' || aks3 == 'y' ){
+        alert('your Answer is yes ');
+    } else {
+        alert('your Answer is no');}
+ 
+ let ask4 = prompt('Is your favorite club is Barcelona?');
+  console.log('yes');
+if (ask4 == 'yes' || ask4 == 'y' ){
+    alert('your Answer is yes ');
+ } else {
+  alert('your Answer is no');}
+
+  let aks5 = prompt('Is Lionel Messi your favorite player?');
+  if (aks5 == 'yes' || aks5 == 'y' ){
+      alert('your Answer is yes ');
+  } else {
+      alert('your Answer is no');}
+
+alert('welcome ' + userName + " thank's for your time");
