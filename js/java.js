abb9e541af20effb9e7ec7afa12362221266292f
abrar189 <algourabrar@gmail.com>
2021-04-26 15:39:54 +0300

    done

--- js/java.js
+++ js/java.js
@@ -2,35 +2,35 @@
 let userName = prompt('what is your name?');
 alert('welcome ' + userName);
 
-let ask1 = prompt('do you like football?');
+let ask1 = prompt('do you like football?').toLowerCase();
 if (ask1 == 'yes' || ask1 == 'y' ){
     console.log('yes');
     alert('your Answer is yes ');
-} else {
+}  else  {
     alert('your Answer is no');}
 
 
-let ask2 = prompt('Is your favorite club is Real Madrid?');
+let ask2 = prompt('Is your favorite club is Real Madrid?').toLowerCase();
     console.log('yes');
 if (ask2 == 'yes' || ask2 == 'y' ){
     alert('your Answer is yes ');
 } else {
     alert('your Answer is no');}
     
-  let aks3 = prompt('Is Cristiano Ronaldo your favorite player?');
+  let aks3 = prompt('Is Cristiano Ronaldo your favorite player?').toLowerCase();
     if (aks3 == 'yes' || aks3 == 'y' ){
         alert('your Answer is yes ');
     } else {
         alert('your Answer is no');}
  
- let ask4 = prompt('Is your favorite club is Barcelona?');
+ let ask4 = prompt('Is your favorite club is Barcelona?').toLowerCase();
   console.log('yes');
 if (ask4 == 'yes' || ask4 == 'y' ){
     alert('your Answer is yes ');
  } else {
   alert('your Answer is no');}
 
-  let aks5 = prompt('Is Lionel Messi your favorite player?');
+  let aks5 = prompt('Is Lionel Messi your favorite player?').toLowerCase();
   if (aks5 == 'yes' || aks5 == 'y' ){
       alert('your Answer is yes ');
   } else {
