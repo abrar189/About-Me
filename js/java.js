407c0f6d9261ccaf431f36159672815e7bdccf3f
abrar189 <algourabrar@gmail.com>
2021-04-27 15:27:35 +0300

    done

--- js/java.js
+++ js/java.js
@@ -1,39 +1,68 @@
 'use strict';
-let userName = prompt('what is your name?');
-alert('welcome ' + userName);
+//let userName = prompt('what is your name?');
+//alert('welcome ' + userName);
 
-let ask1 = prompt('do you like football?').toLowerCase();
-if (ask1 == 'yes' || ask1 == 'y' ){
-    console.log('yes');
-    alert('your Answer is yes ');
-}  else  {
-    alert('your Answer is no');}
+//let ask1 = prompt('do you like football?').toLowerCase();
+//if (ask1 == 'yes' || ask1 == 'y' ){
+   // console.log('yes');
+    //alert('your Answer is yes ');
+//}  else  {
+    //alert('your Answer is no');}
 
 
-let ask2 = prompt('Is your favorite club is Real Madrid?').toLowerCase();
-    console.log('yes');
-if (ask2 == 'yes' || ask2 == 'y' ){
-    alert('your Answer is yes ');
-} else {
-    alert('your Answer is no');}
+//let ask2 = prompt('Is your favorite club is Real Madrid?').toLowerCase();
+   // console.log('yes');
+//if (ask2 == 'yes' || ask2 == 'y' ){
+   // alert('your Answer is yes ');
+//} else {
+   // alert('your Answer is no');}
     
-  let aks3 = prompt('Is Cristiano Ronaldo your favorite player?').toLowerCase();
-    if (aks3 == 'yes' || aks3 == 'y' ){
-        alert('your Answer is yes ');
-    } else {
-        alert('your Answer is no');}
+  //let aks3 = prompt('Is Cristiano Ronaldo your favorite player?').toLowerCase();
+   // if (aks3 == 'yes' || aks3 == 'y' ){
+      //  alert('your Answer is yes ');
+    //} else {
+       // alert('your Answer is no');}
  
- let ask4 = prompt('Is your favorite club is Barcelona?').toLowerCase();
-  console.log('yes');
-if (ask4 == 'yes' || ask4 == 'y' ){
-    alert('your Answer is yes ');
- } else {
-  alert('your Answer is no');}
+// let ask4 = prompt('Is your favorite club is Barcelona?').toLowerCase();
+//  console.log('yes');
+//if (ask4 == 'yes' || ask4 == 'y' ){
+ //   alert('your Answer is yes ');
+ //} else {
+ // alert('your Answer is no');}
 
-  let aks5 = prompt('Is Lionel Messi your favorite player?').toLowerCase();
-  if (aks5 == 'yes' || aks5 == 'y' ){
-      alert('your Answer is yes ');
-  } else {
-      alert('your Answer is no');}
+ // let aks5 = prompt('Is Lionel Messi your favorite player?').toLowerCase();
+ // if (aks5 == 'yes' || aks5 == 'y' ){
+  //    alert('your Answer is yes ');
+ // } else {
+  //    alert('your Answer is no');}
 
-alert('welcome ' + userName + " thank's for your time");
+//alert('welcome ' + userName + " thank's for your time");
+
+//alert( 'you guess a number it must be high')
+//let x=1000
+//let ask6 = prompt('now, can you guess a number?');
+//for (let i = 1; i < 4; i++) {
+  //  if (ask6 == 1000){
+  //  alert('you are hero');
+  //  break;}
+// else {
+  //      ask6 = prompt('now, can you guess a number?');}
+//} 
+//alert('correct number is 1000')
+    
+let ask7 =prompt('How many players in the football team ?');
+let players = [11,14,10,20];
+
+for (let j =0; j< players.length ;j++){
+    if (ask7 == players[j]){
+        break;
+    }else{
+        for (let i=1 ;i<=6 ;i++){
+            ask7 =prompt('How many players in the football team ?');
+            break;
+        }
+    }
+}
+
+
+         
